1cb0eac473678f70a6052d1dd6a42f868e59d832
b4stet <ptitgnu404@gmail.com>
2021-06-05 19:35:40 +0200

    added macb theory in systems.

--- facs/command/systems.py
+++ facs/command/systems.py
@@ -23,6 +23,11 @@ class SystemsCommand(AbstractCommand):
             callback=self.get_notes_windows
         ))
 
+        group.add_command(click.Command(
+            name='macb_theory', help='notes about macb timestamp rules ... well, the theory :)',
+            callback=self.get_notes_macb
+        ))
+
         return group
 
     def get_notes_unix(self):
@@ -39,13 +44,30 @@ class SystemsCommand(AbstractCommand):
         self._print_text('References', self._data['windows']['references'])
 
         fs_ntfs = []
-        for elt in self._data['windows']['ntfs']:
+        for elt in self._data['windows']['fs_ntfs']:
             line = '{:40}: {}'.format(elt['name'], elt['description'])
             fs_ntfs.append(line)
         self._print_text('Cheat Sheet', fs_ntfs)
 
         fs_fat = []
-        for elt in self._data['windows']['fat']:
+        for elt in self._data['windows']['fs_fat']:
             line = '{:40}: {}'.format(elt['name'], elt['description'])
             fs_fat.append(line)
         self._print_text('Cheat Sheet', fs_fat)
+
+    def get_notes_macb(self):
+        self._print_text('References', self._data['macb_theory']['references'])
+
+        win10 = self._data['macb_theory']['ntfs_win10']
+        rules = []
+        for rule in win10:
+            line = '$FN: {:20} $SI: {:40} desc: {}'.format(rule['file_name'], rule['std_info'], rule['description'])
+            rules.append(line)
+        self._print_text('Theoretical rules on NTFS - tested on Windows 10 build 1903', rules)
+
+        win78 = self._data['macb_theory']['ntfs_win7_8']
+        rules = []
+        for rule in win78:
+            line = '$FN: {:20} $SI: {:40} desc: {}'.format(rule['file_name'], rule['std_info'], rule['description'])
+            rules.append(line)
+        self._print_text('Theoretical rules on NTFS - Windows 7/8', rules)
