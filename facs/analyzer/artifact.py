96511e16084211a427a7c483b7553900a7ca55b2
b4stet <ptitgnu404@gmail.com>
2021-09-09 12:14:38 +0200

    fixed extraction of prefetch first run date.

--- facs/analyzer/artifact.py
+++ facs/analyzer/artifact.py
@@ -11,13 +11,18 @@ class ArtifactAnalyzer(AbstractAnalyzer):
             prefetch_name = execution['filename']
 
             if prefetch_name not in prefetchs.keys():
-                prefetchs[prefetch_name] = {}
+                prefetchs[prefetch_name] = {
+                    'oldest_recorded_execution': None,
+                }
 
-            if execution['timestamp_desc'] == 'Creation Time':
-                prefetchs[prefetch_name]['first_execution'] = self._filetime_to_datetime(execution['date_time']['timestamp'])
+            # because of plaso timestamp in 'Creation Time' is wrong
+            if execution['timestamp_desc'] == 'Previous Last Time Executed':
+                exec_time = self._filetime_to_datetime(execution['date_time']['timestamp'])
+                if prefetchs[prefetch_name]['oldest_recorded_execution'] is None or exec_time < prefetchs[prefetch_name]['oldest_recorded_execution']:
+                    prefetchs[prefetch_name]['oldest_recorded_execution'] = exec_time
 
             if execution['timestamp_desc'] == 'Last Time Executed':
-                prefetchs[prefetch_name]['last_execution'] = self._filetime_to_datetime(execution['date_time']['timestamp'])
+                prefetchs[prefetch_name]['earliest_execution'] = self._filetime_to_datetime(execution['date_time']['timestamp'])
                 prefetchs[prefetch_name]['nb_executions'] = execution['run_count']
                 prefetchs[prefetch_name]['mapped_files'] = execution['mapped_files']
 
@@ -27,8 +32,8 @@ class ArtifactAnalyzer(AbstractAnalyzer):
             for file in info['mapped_files']:
                 prefetchs_flattened.append({
                     'prefetch_path': prefetch,
-                    'first_execution': str(info['first_execution']),
-                    'last_execution': str(info['last_execution']),
+                    'oldest_recorded_execution': str(info['oldest_recorded_execution']) if info['nb_executions'] > 1 else str(info['earliest_execution']),
+                    'earliest_execution': str(info['earliest_execution']),
                     'nb_executions': info['nb_executions'],
                     'mapped_file': file,
                 })
