6b7fed3ece856f78d3dee320c96fc9e97cee4e1b
b4stet <ptitgnu404@gmail.com>
2021-10-17 15:48:02 +0200

    add all exec times in prefetch analysis output.

--- facs/analyzer/artifact.py
+++ facs/analyzer/artifact.py
@@ -13,6 +13,7 @@ class ArtifactAnalyzer(AbstractAnalyzer):
             if prefetch_name not in prefetchs.keys():
                 prefetchs[prefetch_name] = {
                     'first_known_execution': None,
+                    'executions_time': []
                 }
 
             # plaso timestamp in 'Creation Time' relates to volumne creation time (not first execution)
@@ -20,12 +21,15 @@ class ArtifactAnalyzer(AbstractAnalyzer):
                 exec_time = self._filetime_to_datetime(execution['date_time']['timestamp'])
                 if prefetchs[prefetch_name]['first_known_execution'] is None or exec_time < prefetchs[prefetch_name]['first_known_execution']:
                     prefetchs[prefetch_name]['first_known_execution'] = exec_time
+                    prefetchs[prefetch_name]['executions_time'].append(str(exec_time))
 
             if execution['timestamp_desc'] == 'Last Time Executed':
-                prefetchs[prefetch_name]['last_known_execution'] = self._filetime_to_datetime(execution['date_time']['timestamp'])
+                exec_time = self._filetime_to_datetime(execution['date_time']['timestamp'])
+                prefetchs[prefetch_name]['last_known_execution'] = exec_time
                 prefetchs[prefetch_name]['nb_executions'] = execution['run_count']
                 prefetchs[prefetch_name]['exe_path'] = ';'.join(execution['path_hints'])
                 prefetchs[prefetch_name]['mapped_files'] = execution['mapped_files']
+                prefetchs[prefetch_name]['executions_time'].append(str(exec_time))
 
         # flatten results per mapped file
         prefetchs_flattened = []
@@ -37,6 +41,7 @@ class ArtifactAnalyzer(AbstractAnalyzer):
                     'first_known_execution': str(info['first_known_execution']) if info['nb_executions'] > 1 else str(info['last_known_execution']),
                     'last_known_execution': str(info['last_known_execution']),
                     'nb_executions': info['nb_executions'],
+                    'exec_times': ';'.join(info['executions_time']),
                     'mapped_file': file,
                 })
 
